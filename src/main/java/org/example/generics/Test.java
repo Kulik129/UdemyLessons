1f22afe410e096ab2b7f77d3946b3469c1c06cc1
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-09-25 12:44:32 +0300

    дженерики

---
+++ src/main/java/org/example/generics/Test.java
@@ -0,0 +1,37 @@
+package org.example.generics;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        Info<String> info = new Info<>("Hello");
+        System.out.println(info.getValue()); // Hello
+        System.out.println(info); // Info {value = Hello}       (@Override toString)
+
+        Info<Integer> info1 = new Info<>(100);
+        System.out.println(info1);
+    }
+}
+
+/**
+ * Parameterized class
+ * @param <T> подставляемый тип данных который потом будет использован при создании объекта
+ */
+class Info<T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info {" +
+                "value = " + value +
+                '}';
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
